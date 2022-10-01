ffcef6322150429d7868015482bd7536b14621cf
Pierre <pierre@advency.fr>
2022-10-01 16:43:27 +0200

    More wall textures

--- layout/Viewport.gd
+++ layout/Viewport.gd
@@ -2,52 +2,53 @@ extends Control
 
 onready var ceilingSprite = $Ceiling
 onready var floorSprite   = $Floor
-onready var wallFront     = $Walls/WallFront
-onready var wallLeft      = $Walls/WallLeft
-onready var wallRight     = $Walls/WallRight
-onready var wallFrontL    = $Walls/WallFrontL
-onready var wallFrontR    = $Walls/WallFrontR
-onready var wallFrontU    = $Walls/WallFrontU
-onready var wallFrontUL   = $Walls/WallFrontUL
-onready var wallFrontUR   = $Walls/WallFrontUR
-onready var wallLeftU     = $Walls/WallLeftU
-onready var wallRightU    = $Walls/WallRightU
-onready var wallFrontUU   = $Walls/WallFrontUU
-onready var wallFrontUUL  = $Walls/WallFrontUUL
-onready var wallLeftUUL   = $Walls/WallLeftUUL
-onready var wallFrontUULL = $Walls/WallFrontUULL
-onready var wallFrontUUR  = $Walls/WallFrontUUR
-onready var wallRightUUR  = $Walls/WallRightUUR
-onready var wallFrontUURR = $Walls/WallFrontUURR
-onready var wallLeftUU    = $Walls/WallLeftUU
-onready var wallRightUU   = $Walls/WallRightUU
-onready var wallLeftUUU   = $Walls/WallLeftUUU
-onready var wallRightUUU  = $Walls/WallRightUUU
-
 onready var walls = $Walls
 
 var sprites = {}
 var spriteBasePath = 'res://assets/sprites/layout'
 var spritePath = ''
 var currentLayout = ''
-#var wallNodes = {}
+var wallNodes = {}
 
 func _ready():
 	add_to_group("viewport")
-#	get_walls()
+	get_walls()
 
 # Get all wall nodes
-#func get_walls():
-#	for wall in walls.get_children():
-#		wallNodes[wall.get_name()] = wall
+func get_walls():
+	for wall in walls.get_children():
+		# Process string
+		var wallName = wall.get_name().replace('tU', 't_U').replace('tL', 't_L').replace('tR', 't_R')
+		wallName[0] = 'w'
+		var wallDirCell = wallName
+		var wallDirSprite =  wallName.split("_")[0].replace('Left', 'Side').replace('Right', 'Side')
+		var wallOffsetCell = ''
+		var wallOffsetSprite = ''
+		if wallName.find('_') != -1:
+			var nameSplit = wallName.split("_")
+			wallDirCell = nameSplit[0]
+			wallOffsetCell = nameSplit[1]
+			wallOffsetSprite = wallOffsetCell
+			if wallDirCell == 'wallFront':
+				wallOffsetSprite = wallOffsetSprite.replace('R', '').replace('L', '')
+			if wallDirSprite == 'wallSide':
+				wallOffsetSprite = wallOffsetSprite.replace('UUR', 'UUS').replace('UUL', 'UUS')
+					
+		# Save wall data in object
+		wallNodes[wall.get_name()] = {}
+		wallNodes[wall.get_name()].sprite = wall
+		wallNodes[wall.get_name()].dirCell = wallDirCell
+		wallNodes[wall.get_name()].dirSprite = wallDirSprite
+		wallNodes[wall.get_name()].offsetCell = wallOffsetCell
+		wallNodes[wall.get_name()].offsetSprite = wallOffsetSprite
 
 # Set new layout for sprites
 func update_layout(newLayout):
 	currentLayout = newLayout
 	spritePath = spriteBasePath + '/' + currentLayout + '/'
 	preload_sprites()
-	ceilingSprite.texture = sprites['ceiling']
-	floorSprite.texture = sprites['floor']
+	ceilingSprite.texture = sprites['Ceiling']
+	floorSprite.texture = sprites['Floor']
 
 # Load all sprites from current layout in dictionary for later use
 func preload_sprites():
@@ -57,7 +58,6 @@ func preload_sprites():
 	while true:
 		var file_name = dir.get_next()
 		if file_name == "":
-			#break the while loop when get_next() returns ""
 			break
 		elif file_name.ends_with('.png'):
 			var spriteKey = file_name.replace(".png", "")
@@ -66,53 +66,17 @@ func preload_sprites():
 
 # Update wall visibility and sprite
 func update_walls(data):
-#	for wall in wallNodes.keys():
-#		var wallName = wall
-#		var wallDir = wallName
-#		var wallOffset = ''
-#		if wallName.find('_') != -1:
-#			var nameSplit = wallName.split("_")
-#			wallDir = nameSplit[0]
-#			if wallDir != 'wallFront':
-#				wallOffset = nameSplit[1]
-#		var walllVisibility = data['currentCell' + wallOffset][wallDir]
-#		wallNodes[wall].visible = walllVisibility
-#		if walllVisibility:
-#			wallNodes[wall].texture = sprites[wallDir + data['currentCell' + wallOffset][wallDir + 'Type'] + wallOffset]
-	# Current cell
-	wallFront.visible = data.currentCell.wallFront
-	wallFront.texture = sprites['wallFront' + data.currentCell.wallFrontType]
-	wallLeft.visible  = data.currentCell.wallLeft
-	wallRight.visible = data.currentCell.wallRight
-	# Current cell + L
-	wallFrontL.visible = data.currentCellL.wallFront
-	# Current cell + R
-	wallFrontR.visible = data.currentCellR.wallFront
-	# Current cell + U
-	wallFrontU.visible = data.currentCellU.wallFront
-	wallLeftU.visible  = data.currentCellU.wallLeft
-	wallRightU.visible = data.currentCellU.wallRight
-	# Current cell + U + L
-	wallFrontUL.visible = data.currentCellUL.wallFront
-	# Current cell + U + R
-	wallFrontUR.visible = data.currentCellUR.wallFront
-	# Current cell + U + U
-	wallFrontUU.visible = data.currentCellUU.wallFront
-	wallLeftUU.visible  = data.currentCellUU.wallLeft
-	wallRightUU.visible = data.currentCellUU.wallRight
-	# Current cell + U + U + L
-	wallFrontUUL.visible = data.currentCellUUL.wallFront
-	wallLeftUUL.visible  = data.currentCellUUL.wallLeft
-	# Current cell + U + U + L + L
-	wallFrontUULL.visible = data.currentCellUULL.wallFront
-	# Current cell + U + U + R
-	wallFrontUUR.visible = data.currentCellUUR.wallFront
-	wallRightUUR.visible = data.currentCellUUR.wallRight
-	# Current cell + U + U + R + R
-	wallFrontUURR.visible = data.currentCellUURR.wallFront
-	# Current cell + U + U + U
-	wallLeftUUU.visible  = data.currentCellUUU.wallLeft
-	wallRightUUU.visible = data.currentCellUUU.wallRight
+	for wallName in wallNodes.keys():
+		print(wallName)
+		var wall = wallNodes[wallName]
+		# Set wall sprite
+		var walllVisibility = data['currentCell' + wall.offsetCell][wall.dirCell]
+		wall.sprite.visible = walllVisibility
+		if walllVisibility:
+			if wall.offsetSprite == 'UUU':
+				wall.sprite.texture = sprites['wallSideDefaultUUU']
+			else:
+				wall.sprite.texture = sprites[wall.dirSprite + data['currentCell' + wall.offsetCell][wall.dirCell + 'Type'] + wall.offsetSprite]
 
 func update_ceiling_floor():
 	floorSprite.flip_h   = !floorSprite.flip_h
