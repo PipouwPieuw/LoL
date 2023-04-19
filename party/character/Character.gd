d4494b3075e8017c0e256440caccd7561d91f2d7
Pierre <pierre@advency.fr>
2023-04-19 21:34:44 +0200

    WIP character details + active frame

--- party/character/Character.gd
+++ party/character/Character.gd
@@ -5,13 +5,17 @@ onready var trigger = $Trigger
 onready var gauges = $Gauges
 onready var attack = $Attack
 onready var spell = $Spell
+onready var activeFrame = $ActiveFrame
 
 var data = {}
+var charId
 var _err
 
 func _ready():
 	add_to_group('party')
-	portrait.texture = load("res://assets/sprites/portraits/char" + data.attributes.id + ".png")
+	charId = data.attributes.id
+	hide_active_frame('all')
+	portrait.texture = load("res://assets/sprites/portraits/char" + charId + ".png")
 	_err = trigger.connect("input_event", self, "trigger_event")
 	_err = gauges.connect("input_event", self, "gauges_event")
 	_err = attack.connect("input_event", self, "attack_event")
@@ -19,16 +23,28 @@ func _ready():
 
 func trigger_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton  and event.button_index == BUTTON_LEFT and event.pressed:
-		get_tree().call_group('chardetails', 'display_details', data.attributes.id)
+		set_active_frame()
+		get_tree().call_group('chardetails', 'display_details', charId)
 
 func gauges_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton  and event.button_index == BUTTON_LEFT and event.pressed:
+		set_active_frame()
 		print("DISPLAY VITALS !")
 
 func attack_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton  and event.button_index == BUTTON_LEFT and event.pressed:
+		set_active_frame()
 		print("ATTACK !")
 
 func spell_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton  and event.button_index == BUTTON_LEFT and event.pressed:
+		set_active_frame()
 		print("CAST SPELL !")
+
+func set_active_frame():
+	get_tree().call_group('party', 'hide_active_frame', charId)
+	activeFrame.visible = true;
+
+func hide_active_frame(id):
+	if not id == charId or id == 'all':
+		activeFrame.visible = false;
